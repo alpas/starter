2d0a3069587c3456bc1aa4693b432b7add128c39
Ashok Gelal <ashokgelal@gmail.com>
2020-01-03 19:20:42 -0700

    Update Alpas

--- src/main/kotlin/configs/QueueConfig.kt
+++ src/main/kotlin/configs/QueueConfig.kt
@@ -1,14 +1,14 @@
 package __PACKAGE__.configs
 
 import dev.alpas.Environment
-import dev.alpas.queue.ActiveMQConnection
-import dev.alpas.queue.SyncQueueConnection
+import dev.alpas.queue.activemq.ActiveMQConnection
+import dev.alpas.queue.passthrough.PassThroughQueueConnection
 import dev.alpas.queue.QueueConfig as BaseConfig
 
 @Suppress("unused")
 class QueueConfig(env: Environment) : BaseConfig(env) {
     init {
         addConnection("activemq", lazy { ActiveMQConnection(env) })
-        addConnection("sync", lazy { SyncQueueConnection() })
+        addConnection("sync", lazy { PassThroughQueueConnection() })
     }
 }
