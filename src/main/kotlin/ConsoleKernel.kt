174a89d8f32981d2295ed08a721f1c8ad98cf7cb
Ashok Gelal <ashokgelal@gmail.com>
2020-01-12 11:43:17 -0700

    Upgrade Alpas

--- src/main/kotlin/ConsoleKernel.kt
+++ src/main/kotlin/ConsoleKernel.kt
@@ -1,6 +1,24 @@
 package __PACKAGE__
 
+import dev.alpas.Application
+import dev.alpas.ServiceProvider
 import dev.alpas.console.ConsoleKernel
+import dev.alpas.encryption.EncryptionServiceProvider
+import dev.alpas.hashing.HashServiceProvider
+import dev.alpas.logging.LoggerServiceProvider
+import dev.alpas.ozone.OzoneServiceProvider
+import dev.alpas.routing.RouteServiceProvider
+import kotlin.reflect.KClass
 
 @Suppress("unused")
-class ConsoleKernel : ConsoleKernel()
+class ConsoleKernel : ConsoleKernel() {
+    override fun serviceProviders(app: Application): Iterable<KClass<out ServiceProvider>> {
+        return listOf(
+            LoggerServiceProvider::class,
+            EncryptionServiceProvider::class,
+            HashServiceProvider::class,
+            RouteServiceProvider::class,
+            OzoneServiceProvider::class
+        )
+    }
+}
--- src/main/kotlin/HttpKernel.kt
+++ src/main/kotlin/HttpKernel.kt
@@ -1,6 +1,30 @@
 package __PACKAGE__
 
+import dev.alpas.Application
+import dev.alpas.ServiceProvider
+import dev.alpas.encryption.EncryptionServiceProvider
+import dev.alpas.hashing.HashServiceProvider
 import dev.alpas.http.HttpKernel
+import dev.alpas.logging.LoggerServiceProvider
+import dev.alpas.ozone.OzoneServiceProvider
+import dev.alpas.queue.QueueServiceProvider
+import dev.alpas.routing.RouteServiceProvider
+import dev.alpas.session.SessionServiceProvider
+import dev.alpas.view.ViewServiceProvider
+import kotlin.reflect.KClass
 
 @Suppress("unused")
-class HttpKernel : HttpKernel()
+class HttpKernel : HttpKernel() {
+    override fun serviceProviders(app: Application): Iterable<KClass<out ServiceProvider>> {
+        return listOf(
+            LoggerServiceProvider::class,
+            EncryptionServiceProvider::class,
+            HashServiceProvider::class,
+            SessionServiceProvider::class,
+            RouteServiceProvider::class,
+            ViewServiceProvider::class,
+            OzoneServiceProvider::class,
+            QueueServiceProvider::class
+        )
+    }
+}
