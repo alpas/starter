f2101fa3848198e4cbb6cb487696607f14d4080a
Ashok Gelal <ashokgelal@gmail.com>
2020-01-14 11:40:33 -0700

    Add a Welcome VueJS component to make it easy to start for a new user.

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -1,1 +1,11 @@
-console.log("Hello, Alpas!")
+import Vue from 'vue'
+import axios from 'axios'
+
+window.axios = axios
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+Vue.component('welcome', require('./components/Welcome').default)
+
+new Vue({
+    el: '#app'
+})
---
+++ src/main/resources/js/components/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Hello, __APP__NAME!</div>
+</template>
+
+<script>
+    export default {
+        name: "Welcome"
+    }
+</script>
+
+<style scoped>
+
+</style>
