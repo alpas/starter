eedc898c7b470125ac0ee229ca631e420cf0b101
Ashok Gelal <ashokgelal@gmail.com>
2020-04-01 09:57:53 -0600

    Import

--- src/main/kotlin/configs/DatabaseConfig.kt
+++ src/main/kotlin/configs/DatabaseConfig.kt
@@ -1,6 +1,7 @@
 package __PACKAGE__.configs
 
 import dev.alpas.Environment
+import dev.alpas.ozone.ConnectionConfig
 import dev.alpas.ozone.DatabaseConfig
 import dev.alpas.ozone.MySqlConnection
 
@@ -14,6 +15,19 @@ class DatabaseConfig(env: Environment) : DatabaseConfig(env) {
 
     // https://alpas.dev/docs/database-getting-started#multiple-database-connections
     private fun addConnections(env: Environment) {
-        addConnection("mysql", lazy { MySqlConnection(env, ConnectionConfig(extraParams = mapOf("useUnicode" to true, "characterEncoding" to "UTF-8", "allowPublicKeyRetrieval" to "true"))) })
+        addConnection(
+            "mysql",
+            lazy {
+                MySqlConnection(
+                    env,
+                    ConnectionConfig(
+                        extraParams = mapOf(
+                            "useUnicode" to true,
+                            "characterEncoding" to "UTF-8",
+                            "allowPublicKeyRetrieval" to "true"
+                        )
+                    )
+                )
+            })
     }
 }
